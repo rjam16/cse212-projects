85a2ff22f227d19173ffd68d8dd614304ab7af0d
rjam16 <jam23019@byui.edu>
2025-01-11 01:09:56 -0800

    Complete teach and code sections of wk1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
 public static class Arrays
 {
     /// <summary>
@@ -12,8 +14,14 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
-
-        return []; // replace this return statement with your own
+        var multi_array = new double [length];
+        // set up fixed array that results can be put into
+        for (int i = 0; i < length; ++i) {
+        // set up loop for each index in the array
+            multi_array[i] = number * (i + 1);
+        //multiply the number given incrementally to return multiples, assign value to given index of the array
+        }
+        return multi_array; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +37,21 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        var moved_left = data.GetRange(data.Count - amount, amount);
+        //get list of data from the end of data to the point of the shift
+        data.RemoveRange(data.Count - amount, amount);
+        //remove retrieved range from data(it is currently stored as moved_left)
+        //results.AddRange(moved_left);
+        //add previously made data into the resulting array
+        //var moved_right = data.GetRange(0, data.Count - amount);
+        data.InsertRange(0, moved_left);
+        //gather remaining data from data array
+        //results.AddRange(moved_right);
+        //add remaining data to resulting array
+        return;
+
+
+
+        }
+
     }
-}
--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -18,6 +18,12 @@ public static class Divisors {
     /// <returns>List of divisors</returns>
     private static List<int> FindDivisors(int number) {
         List<int> results = new();
+        for (int i=1; i< number; ++i) {
+            if (number % i == 0) {
+                results.Add(i);
+            }
+
+        }
         // TODO problem 1
         return results;
     }
